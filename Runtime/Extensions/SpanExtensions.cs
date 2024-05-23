fd11d3c2e7a309f375b3ea9ecb1c62a5f2c16906
Ivan Smolovic <ivansm@nordeus.com>
2024-05-23 21:23:44 +0200

    Update for newer Unity versions

--- Runtime/Extensions/SpanExtensions.cs
+++ Runtime/Extensions/SpanExtensions.cs
@@ -1,97 +1,99 @@
-﻿#if COMPILERSERVICES_UNSAFE
+﻿// #if COMPILERSERVICES_UNSAFE
+
+using System;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
 namespace SolidUtilities
 {
-    using System;
-    using System.Runtime.CompilerServices;
-    using JetBrains.Annotations;
+	// Reduced-down and slightly refactored version of https://gist.github.com/LordJZ/92b7decebe52178a445a0b82f63e585a
+	// It exposes only (T separator) overload of the Split method which was enough for my needs.
+	public static class SpanExtensions
+	{
+		public readonly ref struct Enumerable<T>
+			where T : IEquatable<T>
+		{
+			private readonly ReadOnlySpan<T> _span;
+			private readonly T _separator;
 
-    // Reduced-down and slightly refactored version of https://gist.github.com/LordJZ/92b7decebe52178a445a0b82f63e585a
-    // It exposes only (T separator) overload of the Split method which was enough for my needs.
-    public static class SpanExtensions
-    {
-        public readonly ref struct Enumerable<T>
-            where T : IEquatable<T>
-        {
-            private readonly ReadOnlySpan<T> _span;
-            private readonly T _separator;
+			public Enumerable(ReadOnlySpan<T> span, T separator)
+			{
+				_span = span;
+				_separator = separator;
+			}
 
-            public Enumerable(ReadOnlySpan<T> span, T separator)
-            {
-                _span = span;
-                _separator = separator;
-            }
+			[PublicAPI]
+			public Enumerator<T> GetEnumerator() => new(_span, _separator);
+		}
 
-            [PublicAPI]
-            public Enumerator<T> GetEnumerator() => new Enumerator<T>(_span, _separator);
-        }
+		public ref struct Enumerator<T>
+			where T : IEquatable<T>
+		{
+			private const int SeparatorLength = 1;
+			private readonly ReadOnlySpan<T> _trailingEmptyItemSentinel;
 
-        public ref struct Enumerator<T>
-            where T : IEquatable<T>
-        {
-            private const int SeparatorLength = 1;
-            private readonly ReadOnlySpan<T> _trailingEmptyItemSentinel;
+			private readonly T _separator;
+			private ReadOnlySpan<T> _span;
 
-            private readonly T _separator;
-            private ReadOnlySpan<T> _span;
-            private ReadOnlySpan<T> _current;
+			public Enumerator(ReadOnlySpan<T> span, T separator)
+			{
+				_span = span;
+				_separator = separator;
+				Current = default;
+				_trailingEmptyItemSentinel = Unsafe.As<T[]>(nameof(_trailingEmptyItemSentinel)).AsSpan();
 
-            public Enumerator(ReadOnlySpan<T> span, T separator)
-            {
-                _span = span;
-                _separator = separator;
-                _current = default;
-                _trailingEmptyItemSentinel = Unsafe.As<T[]>(nameof(_trailingEmptyItemSentinel)).AsSpan();
+				if (_span.IsEmpty)
+					TrailingEmptyItem = true;
+			}
 
-                if (_span.IsEmpty)
-                    TrailingEmptyItem = true;
-            }
+			[PublicAPI]
+			public ReadOnlySpan<T> Current { get; private set; }
 
-            [PublicAPI]
-            public ReadOnlySpan<T> Current => _current;
+			private bool TrailingEmptyItem
+			{
+				get => _span == _trailingEmptyItemSentinel;
+				set => _span = value ? _trailingEmptyItemSentinel : default;
+			}
 
-            private bool TrailingEmptyItem
-            {
-                get => _span == _trailingEmptyItemSentinel;
-                set => _span = value ? _trailingEmptyItemSentinel : default;
-            }
+			[PublicAPI]
+			public bool MoveNext()
+			{
+				if (TrailingEmptyItem)
+				{
+					TrailingEmptyItem = false;
+					Current = default;
+					return true;
+				}
 
-            [PublicAPI]
-            public bool MoveNext()
-            {
-                if (TrailingEmptyItem)
-                {
-                    TrailingEmptyItem = false;
-                    _current = default;
-                    return true;
-                }
+				if (_span.IsEmpty)
+				{
+					_span = Current = default;
+					return false;
+				}
 
-                if (_span.IsEmpty)
-                {
-                    _span = _current = default;
-                    return false;
-                }
+				var idx = _span.IndexOf(_separator);
+				if (idx < 0)
+				{
+					Current = _span;
+					_span = default;
+				}
+				else
+				{
+					Current = _span.Slice(0, idx);
+					_span = _span.Slice(idx + SeparatorLength);
+					if (_span.IsEmpty)
+						TrailingEmptyItem = true;
+				}
 
-                int idx = _span.IndexOf(_separator);
-                if (idx < 0)
-                {
-                    _current = _span;
-                    _span = default;
-                }
-                else
-                {
-                    _current = _span.Slice(0, idx);
-                    _span = _span.Slice(idx + SeparatorLength);
-                    if (_span.IsEmpty)
-                        TrailingEmptyItem = true;
-                }
+				return true;
+			}
+		}
 
-                return true;
-            }
-        }
-
-        [Pure, PublicAPI]
-        public static Enumerable<T> Split<T>(this ReadOnlySpan<T> span, T separator)
-            where T : IEquatable<T> => new Enumerable<T>(span, separator);
-    }
+		[Pure]
+		[PublicAPI]
+		public static Enumerable<T> Split<T>(this ReadOnlySpan<T> span, T separator)
+			where T : IEquatable<T> =>
+			new(span, separator);
+	}
 }
-#endif
+// #endif
--- Runtime/Helpers/GUIHelper.cs
+++ Runtime/Helpers/GUIHelper.cs
@@ -1,30 +1,41 @@
-﻿namespace SolidUtilities
-{
-    using System;
-    using JetBrains.Annotations;
-    using UnityEngine;
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
 
-    public static class GUIHelper
-    {
-        private static readonly GUIStyle _closeButtonStyle = GUI.skin.FindStyle("ToolbarSeachCancelButton");
+namespace SolidUtilities
+{
+	public static class GUIHelper
+	{
+		public readonly struct ScrollView : IDisposable
+		{
+			public ScrollView(Rect position, ref Vector2 scrollPosition, Rect viewRect)
+			{
+				scrollPosition = GUI.BeginScrollView(position, scrollPosition, viewRect);
+			}
+
+			public void Dispose() => GUI.EndScrollView();
+		}
+
+		private static readonly GUIStyle _closeButtonStyle = GUI.skin.FindStyle("ToolbarSearchCancelButton");
 
         /// <summary>Draws the close button.</summary>
         /// <param name="buttonRect">Rect the button should be located in.</param>
         /// <returns>Whether the button was pressed.</returns>
-        /// <example><code>
+        /// <example>
+        ///     <code>
         /// if (DrawHelper.CloseButton(buttonRect))
         /// {
         ///     searchText = string.Empty;
         ///     GUI.FocusControl(null);
         /// }
-        /// </code></example>
-        [PublicAPI] public static bool CloseButton(Rect buttonRect)
-        {
-            // This is a known problem that the button does not align to center horizontally for some reason.
-            // I tried alignment = TextAnchor.MiddleCenter, setting padding and margin to different values,
-            // but to no avail. Any help with this is appreciated.
-            return GUI.Button(buttonRect, GUIContent.none, _closeButtonStyle);
-        }
+        /// </code>
+        /// </example>
+        [PublicAPI]
+		public static bool CloseButton(Rect buttonRect) =>
+			// This is a known problem that the button does not align to center horizontally for some reason.
+			// I tried alignment = TextAnchor.MiddleCenter, setting padding and margin to different values,
+			// but to no avail. Any help with this is appreciated.
+			GUI.Button(buttonRect, GUIContent.none, _closeButtonStyle);
 
         /// <summary>
         /// Draws content in scroll view.
@@ -32,16 +43,6 @@
         /// <param name="position">Rectangle on the screen to use for the ScrollView.</param>
         /// <param name="scrollPosition">The pixel distance that the view is scrolled in the X and Y directions.</param>
         /// <param name="viewRect">The rectangle used inside the scrollView.</param>
-        public static ScrollView ScrollViewBlock(Rect position, ref Vector2 scrollPosition, Rect viewRect) => new ScrollView(position, ref scrollPosition, viewRect);
-
-        public readonly struct ScrollView : IDisposable
-        {
-            public ScrollView(Rect position, ref Vector2 scrollPosition, Rect viewRect)
-            {
-                scrollPosition = GUI.BeginScrollView(position, scrollPosition, viewRect);
-            }
-
-            public void Dispose() => GUI.EndScrollView();
-        }
-    }
+        public static ScrollView ScrollViewBlock(Rect position, ref Vector2 scrollPosition, Rect viewRect) => new(position, ref scrollPosition, viewRect);
+	}
 }
